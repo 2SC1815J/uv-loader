10379a0b171a0674139b132780a248acf664c962
2SC1815J <2SC1815J@users.noreply.github.com>
2018-01-24 18:00:44 +0900

    add support for a drop of 'IIIF drag and drop' link (manifest with canvas)

--- index.js
+++ index.js
@@ -7,46 +7,64 @@
 (function() {
     'use strict';
 
-    var manifestUri;
-    var canvasUri;
-
-    var match = location.search.match(/(?:&|\?)manifest=(.+?)(?:&|$)/);
-    if (match) {
-        manifestUri = decodeURIComponent(match[1]);
-        match = location.search.match(/(?:&|\?)canvas=(.+?)(?:&|$)/);
-        if (match) {
-            canvasUri = decodeURIComponent(match[1]);
+    processUri(location.search);
+    
+    $(document).on('dragover', function(e){
+        e.stopPropagation();
+        e.preventDefault();
+    });
+    $(document).on('drop', function(e) {
+        e.preventDefault();
+        var url = e.originalEvent.dataTransfer.getData('URL');
+        if (url && url.indexOf('?') > -1) {
+            processUri(url.substring(url.indexOf('?')));
         }
-    }
-    if (manifestUri) {
-        $.getJSON(manifestUri, function(manifest) {
-            var canvasIndex = 0;
-            var sequenceIndex = 0;
-            if (canvasUri) {
-                var canvasUri_ = canvasUri.replace(/^https?:/, '');
-                try {
-                    for (var j = 0; j < manifest.sequences.length; j++) {
-                        for (var i = 0; i < manifest.sequences[j].canvases.length; i++) {
-                            if (manifest.sequences[j].canvases[i]['@id'].replace(/^https?:/, '') === canvasUri_) {
-                                canvasIndex = i;
-                                sequenceIndex = j;
-                                break;
-                            }
-                        }
-                    }
-                } catch (e) {
-                    //
+    });
+
+    function processUri(uri) {
+        if (uri) {
+            var manifestUri;
+            var canvasUri;
+            
+            var match = uri.match(/(?:&|\?)manifest=(.+?)(?:&|$)/);
+            if (match) {
+                manifestUri = decodeURIComponent(match[1]);
+                match = uri.match(/(?:&|\?)canvas=(.+?)(?:&|$)/);
+                if (match) {
+                    canvasUri = decodeURIComponent(match[1]);
                 }
             }
-            $('.uv').attr('data-uri', manifestUri);
-            $('.uv').attr('data-canvasindex', canvasIndex);
-            $('.uv').attr('data-sequenceindex', sequenceIndex);
-            loadViewer();
-            if (history && history.replaceState && history.state !== undefined) {
-                var newUrl = '?manifest=' + manifestUri;
-                history.replaceState(null, document.title, newUrl);
+            if (manifestUri) {
+                $.getJSON(manifestUri, function(manifest) {
+                    var canvasIndex = 0;
+                    var sequenceIndex = 0;
+                    if (canvasUri) {
+                        var canvasUri_ = canvasUri.replace(/^https:/, 'http:');
+                        try {
+                            for (var j = 0; j < manifest.sequences.length; j++) {
+                                for (var i = 0; i < manifest.sequences[j].canvases.length; i++) {
+                                    if (manifest.sequences[j].canvases[i]['@id'].replace(/^https:/, 'http:') === canvasUri_) {
+                                        canvasIndex = i;
+                                        sequenceIndex = j;
+                                        break;
+                                    }
+                                }
+                            }
+                        } catch (e) {
+                            //
+                        }
+                    }
+                    $('.uv').attr('data-uri', manifestUri);
+                    $('.uv').attr('data-canvasindex', canvasIndex);
+                    $('.uv').attr('data-sequenceindex', sequenceIndex);
+                    loadViewer();
+                    if (history && history.replaceState && history.state !== undefined) {
+                        var newUrl = '?manifest=' + manifestUri;
+                        history.replaceState(null, document.title, newUrl);
+                    }
+                });
             }
-        });
+        }
     }
 
     function loadViewer() {
